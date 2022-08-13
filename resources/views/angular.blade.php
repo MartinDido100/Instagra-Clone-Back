2a43580bb9fcc19a369ddbe7401c5ac5ad72e654
Martin Didolich <marindidolich2012@gmail.com>
2022-08-13 20:47:44 -0300

    x

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html><html lang="en"><head>
+  <meta charset="utf-8">
+  <title>Instagram</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/angular/assets/icons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/angular/assets/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/angular/assets/icons/favicon-16x16.png">
+  <link data-default-icon="assets/angular/assets/icons/favicon.ico" rel="sortcut icon" type="image/x-icon" href="assets/angular/assets/icons/favicon.ico">
+  <link rel="manifest" href="assets/angular/assets/icons/site.webmanifest">">
+<style>*{margin:0;padding:0;box-sizing:border-box}body{width:100%;font-size:100%;line-height:1.4em;min-height:100vh;background-color:#fff;hyphens:auto;font-smooth:always;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}*{font-family:-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif}:root{--primary-blue:#0095f6;--grey-font-color:#8e8e8e}body{background:#fafafa;width:100%;min-height:100vh}</style><link rel="stylesheet" href="/assets/angular/styles.0044271dc40e6404.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/assets/angular/styles.0044271dc40e6404.css"></noscript></head>
+<body>
+  <app-root></app-root>
+<script src="/assets/angular/runtime.77253f2ee0b7fd6d.js" type="module"></script><script src="/assets/angular/polyfills.b0ee159513a070f4.js" type="module"></script><script src="/assets/angular/main.3d56ee0582d07499.js" type="module"></script>
+
+</body></html>
+
+
