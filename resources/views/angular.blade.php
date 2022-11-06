989c6292d796f886d04bfde9ab6518a408cc353f
Martin Didolich <marindidolich2012@gmail.com>
2022-11-06 15:33:28 -0300

    Icon bug resolved

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -7,6 +7,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="assets/angular/assets/icons/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="assets/angular/assets/icons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="assets/angular/assets/icons/favicon-16x16.png">
+<link rel="shortcut icon" href="assets/angular/assets/icons/favicon.ico" type="image/ico"/>
 <link rel="manifest" href="assets/angular/assets/icons/site.webmanifest">
 <style>*{margin:0;padding:0;box-sizing:border-box}body{width:100%;font-size:100%;line-height:1.4em;min-height:100vh;background-color:#fff;hyphens:auto;font-smooth:always;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}*{font-family:-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif}:root{--primary-blue:#0095f6;--grey-font-color:#8e8e8e}body{background:#fafafa;width:100%;min-height:100vh}</style><link rel="stylesheet" href="/assets/angular/styles.0044271dc40e6404.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/assets/angular/styles.0044271dc40e6404.css"></noscript></head>
 <body>
